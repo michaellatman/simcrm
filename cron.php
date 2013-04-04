7785f553b7c3953f23a3bc2d84386b2b5e6d887d
Michael Latman <michael@latman.org>
2013-04-03 21:50:28 -0400

    Initial commit

---
+++ cron.php
@@ -0,0 +1,82 @@
+<?php
+/*
+Created, designed, and implemented by Michael Latman 
+© Copyright 2012 Dugley Labs. This product was made under contract of Kraft estates and should not be distributed to 3rd parties without the prior consent of the creator.
+*/
+
+//SELECT * FROM `visitors` WHERE NOW() > DATE_ADD(`last_seen`, INTERVAL 36 MINUTE) LIMIT 0, 30
+
+
+$conn = new Mongo("mongodb://kraft:awdadw34@flame.mongohq.com:27103/CRMSimLegacy");
+$db = $conn->CRMSimLegacy;
+
+
+$collection = $db->payments;
+$ago = new DateTime();
+$ago->modify('-1 month');
+$criteria = array('paid' => array('$lte' => $ago));
+$cursor = $collection->find($criteria);
+foreach ($cursor as $doc) { 
+	if($doc['account'] != 'CRMSuite'){
+		$hcollection = $db->teams;
+		$d = $hcollection->findOne(array("_id" => $doc['account']));
+		$estates = count($d['estates']);
+		$agents = count($d['agents']);
+		if($people > 5){
+			$payment = $estates*6000;
+		}
+		else{
+			$payment = $estates*($agents*1000);
+		}
+		$payment =1;
+		
+		if($doc['amount'] >= $payment){
+			$doc['amount'] -= $payment;
+			$doc['disabled'] = FALSE;
+			$doc['paid'] = new DateTime();
+			$collection->save($doc);
+			
+		}
+		else{
+			if($doc['disabled'] == FALSE){
+				$mcollection = $db->payments;
+				$mque = $mcollection->findOne(array('account' => 'CRMSuite',));
+				$mque['messages'] .= '&&'.$d['contact_uuid'].'*Your account has been disabled.';
+				$doc['disabled'] = TRUE;
+				$collection->save($doc);
+				$mcollection->save($mque);
+			}
+
+		}
+	}
+}
+
+
+$collection = $db->visitors;
+
+
+$tenMinutesAgo = new DateTime();
+$tenMinutesAgo->modify('-15 minutes');
+	$search = array('updated' => array('$lte' => $tenMinutesAgo));
+$cursor = $collection->remove($search);
+
+
+$collection = $db->users;
+$vcollection = $db->visitors;
+$oneMinutesAgo = new DateTime();
+$oneMinutesAgo->modify('-1 minutes');
+	$search = array('updated' => array('$ne' => null, '$lte' => $oneMinutesAgo));
+$cursor = $collection->find($search);
+foreach ($cursor as $doc) { 
+	$search = array('locked_by' => $doc['key']);
+	$find = $vcollection->find($search);
+	foreach ($find as $doc2) {
+		$doc2['locked_by'] = null;
+		$vcollection->save($doc2);
+	}
+	$doc['last_seen'] = $doc['updated'];
+	$doc['updated'] = null;
+	$collection->save($doc);
+}
+
+?>
---
+++ hud-mongo.php
@@ -0,0 +1,238 @@
+<?php
+/*
+Created, designed, and implemented by Michael Latman 
+© Copyright 2012 Dugley Labs. This product was made under contract of Kraft estates and should not be distributed to 3rd parties without the prior consent of the creator.
+*/
+
+$conn = new Mongo("mongodb://kraft:awdadw34@flame.mongohq.com:27103/CRMSimLegacy");
+
+$db = $conn->CRMSimLegacy;
+include('sql.php');
+//SELECT * FROM `visitors` WHERE NOW() > DATE_ADD(`last_seen`, INTERVAL 36 MINUTE) LIMIT 0, 30
+
+
+$motd = 'Connected to network. Please note that at some point next week we will be migrating to the new sever scripts. This transition is going to be as seamless as possible. Please stay tuned.';
+
+	$dbd = $GLOBALS;
+		$headers 		= $dbd['HTTP_ENV_VARS'];
+		$objectgrid 	= $headers["HTTP_X_SECONDLIFE_SHARD"]; 
+		$objectname 	= $headers["HTTP_X_SECONDLIFE_OBJECT_NAME"];
+		$objectkey     	= $headers["HTTP_X_SECONDLIFE_OBJECT_KEY"];
+		$ownerkey     	= $headers["HTTP_X_SECONDLIFE_OWNER_KEY"];
+		$objectpos     	= $headers["HTTP_X_SECONDLIFE_LOCAL_POSITION"];
+		$ownername 	= $headers["HTTP_X_SECONDLIFE_OWNER_NAME"];
+		$regiondata     = $headers["HTTP_X_SECONDLIFE_REGION"];
+		$regiontmp 	= explode ("(",$regiondata); 
+		$regionpos 	= explode (")",$regiontmp[1]);
+		$regionname 	= substr($regiontmp[0],0,-1);
+
+$bdoc = $db->teams->findOne(array('agents' => $ownername));
+$pdoc = $db->payments->findOne(array('account' => $bdoc['_id']));
+
+function generate_people(){
+global $conn;
+$dbd = $GLOBALS;
+		$headers 		= $dbd['HTTP_ENV_VARS'];
+		$ownername 	= $headers["HTTP_X_SECONDLIFE_OWNER_NAME"];
+//$conn = new Mongo("mongodb://kraft:awdadw34@localhost/Kraft");
+
+$db = $conn->CRMSimLegacy;
+global $pdoc;
+
+echo('people|');
+	if($pdoc['disabled']==FALSE){
+		
+	 
+		
+		
+			$db = $conn->CRMSimLegacy;
+			$collection = $db->visitors;
+			$bdoc = $db->teams->findOne(array('agents' => $ownername));
+			$criteria = array(
+				'$or' => array(array('locked_by' => $_REQUEST['ownerkey']),array('locked_by' => null)),
+				 'estate' => array('$in' => $bdoc['estates'])
+			);
+		
+			$cursor = $collection->find($criteria);
+			$cursor = $cursor->sort(array('locked_by'=>-1));
+			//echo $doc->count() . ' document(s) found. <br/>';
+			$i=0;
+			foreach ($cursor as $doc) {
+				$i+=1;
+				if($i<=15){
+				//List all 	
+				if($doc['name'] != ""){
+				if($doc['locked_by'] ==null) $doc['locked_by'] = " ";
+				$pieces = explode("&&", $doc['location']);
+				if($doc['estate'] != '') $doc['estate'] .= '';
+				echo($doc['name'].'//'.$doc['uuid'].'//'.$doc['uuid'].'//'.$doc['locked_by'].'//'.$doc['location'].'&&'.$doc['estate'].'*');					
+				}
+				}
+			}
+	}
+	else{
+		echo('unauthorized|Can\'t connect. Balance unpaid please contact your team leader..');			
+	}
+}
+
+$method = $_REQUEST['method'];
+if($method == "register"){
+	//$ownername = $_REQUEST['owner'];
+	$db = $conn->CRMSimLegacy;
+	$collection = $db->users;
+	
+	
+	$criteria = array(
+		'name' => $ownername,
+	);
+	$doc = $collection->findOne($criteria);
+	//$bdoc = $db->teams->findOne(array('agents' => $ownername));
+	//$pdoc = $db->payments->findOne(array('account' => $bdoc['_id']));
+	//echo $doc->count() . ' document(s) found. <br/>';
+	if($bdoc['lead']!=""&&$pdoc['disabled']==FALSE){
+		if(count($doc) == 0){
+			$doc['name'] = $ownername;
+			//$db->teams->save($bdoc);
+		}
+		//$collection->update($criteria,array('$set' => array('title' => 'My last post')));
+		//Person is registered
+		$doc['key'] = $ownerkey;
+		$now = new DateTime();
+		$doc['updated'] = $now;
+		
+  		$collection->save($doc);
+  		echo('registered|CRMSuite|'.$motd.""); 	
+	}
+	else{
+		echo('unauthorized|Can\'t connect. Unauthorized.');
+
+	}
+
+}
+else if($method == "lock"){
+	$db = $conn->CRMSimLegacy;
+	$collection = $db->visitors;
+	$mcollection = $db->users;
+	$time = new DateTime();
+	$search = array('uuid' => $_REQUEST['id']);
+	
+	$row = $collection->findOne($search);
+	if($_REQUEST['id'] != ""){
+		if($row['locked_by'] != null){
+			if($row['locked_by'] == $ownerkey){
+				$row['locked_by'] = null;
+				$collection->save($row);
+				generate_people();
+			}
+			else{
+				$search = array('key' => $row['locked_by']);
+				
+				$mrow = $mcollection->findOne($search);
+				echo('say|Sorry, this customer is locked by '.$mrow['name']);
+			}
+		}
+		else{
+			$row['locked_by'] = $ownerkey;
+			$collection->save($row);
+			generate_people();
+		}
+	}
+}
+else if($method == "land-board"){
+	//$db = $conn->CRMSimLegacy;
+	$team = $db->teams->findOne(array("agents" => $ownername));
+	$i;
+	$a = $team['agents'];
+	$num=count($a);
+	for($i=0;$i!=$num+1;$i++){
+		$user = $db->users->findOne(array("name" => $a[$i]));
+		//$d = $user['photo'];
+		//if($d!="")$d='*'.$d;
+		echo $user["key"].'|';
+	
+	}
+	
+}
+else if($method == "get-people"){ 
+	$collection = $db->users;
+	$criteria = array(
+		'name' => $ownername,
+	);
+
+	$doc = $collection->findOne($criteria);
+	if($doc['name'] != ""){
+		$now = new DateTime();
+		$doc['updated'] = $now;	
+		//$doc['last_seen'] = null;	
+	  	$collection->save($doc);
+	  	
+		generate_people();
+	}
+	else{
+		
+	}
+}
+else if($method == "remove-person"){
+	$collection = $db->visitors;
+	$mcollection = $db->users;
+	$time = new DateTime();
+	$search = array('uuid' => $_REQUEST['id']);
+	 
+	$row = $collection->findOne($search);
+ 
+	if($_REQUEST['id'] != ""){
+			if($row['locked_by'] == $ownerkey){
+				$collection->remove($row);
+			}
+	}
+	//generate_people();
+}
+else if($method == "sensor-dropped"){
+	$estate = $db->estates->findOne(array('groups'=>$_REQUEST['landgroup']));
+	if($estate['estate'] != "") $estate['drops']+=1; $db->estates->save($estate);
+}
+else if($method == "sensor-person"){
+	$collection = $db->visitors;
+	$estate = $db->estates->findOne(array('groups'=>$_REQUEST['landgroup']));
+	
+	$time = new DateTime();
+	$search = array('uuid' => $_REQUEST['uuid']);
+	$row = $collection->findOne($search);
+	if($row['uuid'] == ""){
+		if($estate['estate']!=""){
+			$estate['newleads']+=1; $db->estates->save($estate);
+		}
+		$collection->update(array("uuid" => $_REQUEST['uuid']), array('name' => $_REQUEST['name'], 'estate' => $estate['estate'], 'location' => $_REQUEST['location'], 'uuid' => $_REQUEST['uuid'],'locked_by' => null, 'updated' => $time), array("upsert" => true));
+		echo('say|Welcome, a sales representative should be with you shortly.');
+	}
+	else{
+		$agentname = $db->users->findOne(array('key'=>$row['locked_by']));
+
+		$bdoc = $db->teams->findOne(array('agents' => $agentname['name']));
+		
+		//$pdoc = $db->payments->findOne(array('account' => $bdoc['lead']));
+		//echo('sss'.$bdoc['lead']);
+		//echo('say|'. );
+		$row['updated'] = $time;
+		$row['location'] = $_REQUEST['location'];
+		if($row['estate']!=$estate['estate']){ 
+			$row['estate'] = $estate['estate'];
+			//if($estate['estate'])
+			if($bdoc['estates'] != null){ 
+				if(in_array($estate['estate'], $bdoc['estates']) == FALSE){ 
+					$row['locked_by'] = null;
+					$estate['newleads']+=1; $db->estates->save($estate);
+				}
+			
+			}
+		}
+
+		$collection->save($row);
+		//mysql_query("UPDATE visitors SET location = '".$_REQUEST['location']."', uuid = '".$_REQUEST['uuid']."', last_seen = CURRENT_TIMESTAMP  WHERE uuid = '".$_REQUEST['uuid']."'") or die(mysql_error());
+	}
+	 
+}
+
+
+$conn->close();
+?>
