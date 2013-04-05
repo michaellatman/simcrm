2b2506a585a1daefd6d04ab6f15655511a585626
Michael Latman <michael@latman.org>
2013-04-04 22:50:11 -0400

    adw

--- hud-mongo.php
+++ hud-mongo.php
@@ -25,7 +25,7 @@ $headers 	= apache_request_headers();
 		$regionpos 	= explode (")",$regiontmp[1]); //
 		$regionname 	= substr($regiontmp[0],0,-1); // cut last space from simname
 
-$bdoc = $db->teams->find(array('lead' => "Allyson Breumann"));
+$bdoc = $db->teams->findOne(array('lead' => "Allyson Breumann"));
 
 
 $pdoc = $db->payments->findOne(array('account' => $bdoc['_id']));
@@ -61,7 +61,7 @@ echo('people|');
 			$db = $conn->CRMSimLegacy;
 			$collection = $db->visitors;
 			//$bdoc = $db->teams->findOne(array('agents' => $ownername));
-			$bdoc = $db->teams->find(array('lead' => "Allyson Breumann"));
+			$bdoc = $db->teams->findOne(array('lead' => "Allyson Breumann"));
 
 			$criteria = array(
 				'$or' => array(array('locked_by' => $_REQUEST['ownerkey']),array('locked_by' => null)),
