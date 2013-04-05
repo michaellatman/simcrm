74f6b108cd723b640024f494c94d2a1949a36c05
Michael Latman <michael@latman.org>
2013-04-04 20:43:06 -0400

    LOL

--- hud-mongo.php
+++ hud-mongo.php
@@ -209,7 +209,7 @@ else if($method == "admin-add"){
 else if($method == "admin-remove"){
 	$team = $db->teams->findOne(array("agents" => $ownername));
 	$toremove = 'foo';
-	unset($team['agents'][array_search($team['agents'],trim($_REQUEST['person']))]);
+	unset($team['agents'][array_search(trim($_REQUEST['person']),$team['agents'])]);
 	$db->teams->save($team);
 	echo($_REQUEST['person']);
 }
